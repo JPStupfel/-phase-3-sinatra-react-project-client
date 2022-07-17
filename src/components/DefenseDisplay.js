8e045c0f0fd2f3673b1730649addfd7744c14511
John Patrick Stupfel <johnpatrickstupfel@gmail.com>
2022-07-17 14:48:35 -0500

    add new defense button conditionally renders a div in defense display

--- src/components/DefenseDisplay.js
+++ src/components/DefenseDisplay.js
@@ -8,20 +8,6 @@ export default function DefenseDisplay({currentDefense, handleDefensePatchSubmit
 
     const [isEditDefense, setIsEditDefense] = useState(false)
 
-    // function DefenseContentDisplay({currentDefense, handleDeleteDefense}){
-        
-    //     return <div >
-    //     <h1 class="mt-4">Details on this Defense!</h1>
-    //     <p>Defense Name: {currentDefense.name}</p>
-    //     <p>Defense Result: {currentDefense.result}.</p>
-    //     <p>Defense Notes: {currentDefense.notes}</p>
-    //     <button onClick={()=>setIsEditDefense(prev=>!prev)}>Edit This Defense</button>
-    //     <button 
-    //     onClick={()=>handleDeleteDefense(currentDefense)
-    //     }>Delete This Defense</button>
-
-    // </div> }
-    
 
 
 
@@ -37,7 +23,16 @@ export default function DefenseDisplay({currentDefense, handleDefensePatchSubmit
         currentDefense={currentDefense}
         setIsEditDefense={setIsEditDefense}
         handleDefensePatchSubmit={handleDefensePatchSubmit}/>
+        
         :
+        
+        isAddDefense ? 
+        
+        <div>hello</div>
+
+
+        :
+
        <DefenseContentDisplay 
        handleDeleteDefense={handleDeleteDefense}
        currentDefense={currentDefense} />
