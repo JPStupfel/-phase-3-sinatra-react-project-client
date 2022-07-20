1d75c5dd13dd59ea0de734f4c3d54996a2d40366
John Patrick Stupfel <johnpatrickstupfel@gmail.com>
2022-07-20 16:56:49 -0500

    has routes for home and narratives

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
+import React from 'react';
+import { ReactDOM } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Home from './components/Home';
+// import { Link } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
 
 function App() {
   return (
-   <Home />
+    <>
+    {/* <Link to="/">NoteBook</Link> |{" "}
+    <Link to="/Narratives">Narratives</Link> */}
+
+   <BrowserRouter>
+   <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="Narratives" element={<>Narrative</>} />
+   </Routes>
+   </BrowserRouter>
+   
+   </>
+
+   
   );
 }
 
