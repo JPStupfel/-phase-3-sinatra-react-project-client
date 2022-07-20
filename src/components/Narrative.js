633e30d21e1db98f55c49aa15af439d30a6978d5
John Patrick Stupfel <johnpatrickstupfel@gmail.com>
2022-07-20 17:32:59 -0500

    /narratives is route with conditional renders on client side

--- src/components/Narrative.js
+++ src/components/Narrative.js
@@ -1,5 +1,35 @@
 import {React, useState, useEffect} from "react";
 
 export default function Narrative(props){
-    return(<>narratives</>)
+
+    const [narratives, setNarratives] = useState([])
+    const [isList, setIsList] = useState(false)
+
+    useEffect(
+        ()=>{fetch('http://localhost:9292/sequences').then(r=>r.json()).then(d=>setNarratives(d))}, []
+    )
+
+    function Narration({narrative}){return(
+        <li>{narrative}</li>
+
+    )}
+    const narrativeCards = narratives.map(e=>
+        <Narration narrative={e.narrative} key={e.narrative}/>
+    )
+
+    let randomNumber = Math.floor(Math.random() * narratives.length);
+    
+    console.log(randomNumber)
+
+    return(
+        <>
+        <button onClick={()=>setIsList(prev=>!prev)}>
+            {isList ? "Random" : 'List All'}
+            </button>
+            {isList ?
+                <>{narrativeCards} </> : 
+                <ul>{narrativeCards[randomNumber]}</ul>
+             }
+        </>
+    )
 }
